e4f3f7a51e04b96e768bd24657c82bef24896bf6
Wil Jankovic <156036095+w1lj4nk0v1c@users.noreply.github.com>
2025-01-28 15:38:20 +1030

    Revert "Merge branch 'custom-swerve' into auto"
    This reverts commit 21830b2bceaabe92a3cdb6e7ae241ac6e194e78f, reversing
    changes made to 04700458f0560d7e6458fbbd9dbce194fbaa7b47.

--- Official_Official_Bot/Barnacle_4537_2025/src/main/java/frc/robot/Constants.java
+++ Official_Official_Bot/Barnacle_4537_2025/src/main/java/frc/robot/Constants.java
@@ -7,15 +7,11 @@ package frc.robot;
 import edu.wpi.first.math.util.Units;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -26,8 +22,6 @@ public final class Constants {
   }
 
   public static class SwerveConstants {
-    public static final double kp = 0;
-    public static final double ki = 0;
-    public static final double kd = 0;
+    public static final double maxSpeed = Units.feetToMeters(4.5);
   }
 }
