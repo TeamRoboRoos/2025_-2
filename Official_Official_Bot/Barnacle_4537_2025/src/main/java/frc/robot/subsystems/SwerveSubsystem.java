c8a56311c394c6e902cde63f3e444dd7d2ec2212
Zephram-FRC <122358300+Zephram-FRC@users.noreply.github.com>
2025-02-16 11:16:25 +1030

    Add files via upload

--- Official_Official_Bot/Barnacle_4537_2025/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ Official_Official_Bot/Barnacle_4537_2025/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -160,6 +160,7 @@ public class SwerveSubsystem extends SubsystemBase {
       SmartDashboard.putBoolean("Ready To Score", false);
     }
 
+    simplifyGyro();
     SmartDashboard.putNumber("Gyro", getAHRSAngle().getDegrees());
     posePublisher.set(swerveDrive.getPose());
 
@@ -260,7 +261,15 @@ public class SwerveSubsystem extends SubsystemBase {
     swerveDrive.resetOdometry(new Pose2d(pose.getX(), pose.getY(), angle));
 
     getAHRS().setAngleAdjustment(0);
-    getAHRS().setAngleAdjustment(-getAHRS().getAngle());
+  }
+
+  public void simplifyGyro() {
+    double gyro = getAHRSAngle().getDegrees();
+
+    getAHRS().setAngleAdjustment(gyro % 360);
+    if (gyro < 0) {
+      getAHRS().setAngleAdjustment(-gyro);
+    }
   }
 
   // swerveDrive.getGyro().setOffset(new Rotation3d(0, 0, 0));
